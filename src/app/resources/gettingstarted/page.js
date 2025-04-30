5062ae0369873ea9c45e4102e8c765b69c478983
penscom <clebiodesouza22@gmail.com>
2025-04-30 20:57:16 +0100

    -UI/UX updates: 30/04/25

--- src/app/resources/gettingstarted/page.js
+++ src/app/resources/gettingstarted/page.js
@@ -1,13 +1,20 @@
 'use client'
 import HeroSection from "@/app/components/heroSection/herosection"
 import NavBar from "@/app/components/navBar/nav";
-export default function () {
+import { motion } from "framer-motion";
+export default function Home () {
     return (
         <>
             <header
                 id="header"
                 className="w-full flex-col flex relative justify-center md:rounded-3xl md:mb-2 xl:mb-4 overflow-hidden"
             >
+                <motion.div 
+                    className="absolute top-0 left-0 w-full h-full bg-[url('/herosection-image.jpg')] bg-cover bg-fixed opacity-20"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 0.2 }}
+                    transition={{ duration: 1.5 }}
+                />
                 <div className="relative z-10">
                 <NavBar />
                 <HeroSection
